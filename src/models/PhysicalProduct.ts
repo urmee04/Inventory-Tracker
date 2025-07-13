c9d82a92da7728d2474c5ab5c6e82c4aa4231d6b
urmee04 <urmee04sust@gmail.com>
2025-07-13 09:18:29 -0400

    Update/Correct getter method

--- src/models/PhysicalProduct.ts
+++ src/models/PhysicalProduct.ts
@@ -1,12 +1,12 @@
 import { Product } from "./Product";
 // Define the PhysicalProduct class extending the Product class
-class PhysicalProduct extends Product {
+export class PhysicalProduct extends Product {
   // Static tax rate for all physical products
   static taxRate = 0.1;
   weight: number;
   // Constructor to initialize the product with weight
-  constructor(name: string, price: number, sku: string, weight: number) {
-    super(name, price, sku);
+  constructor(sku: string, name: string, price: number, weight: number) {
+    super(sku, name, price);
     this.weight = weight;
   }
   // Override the getPriceWithTax method to include a 10% tax rate
@@ -14,7 +14,7 @@ class PhysicalProduct extends Product {
     return this.price * (PhysicalProduct.taxRate + 1);
   }
   // Getter method to return the formatted weight in kilograms
-  getWeightWithKg(): string {
-    return `(Weight of this product is ${this.weight})kg`;
+  get weightWithKg(): string {
+    return `${this.weight} kg`;
   }
 }
