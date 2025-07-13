832777d75bd10f6f20ba129f5acbc6671cf5145b
urmee04 <urmee04sust@gmail.com>
2025-07-13 09:16:42 -0400

    change displayDetails() string value

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,19 +1,19 @@
 // Base class with properties initialized via constructor
 
-class Product {
+export class Product {
   /**
    * Static tax rate (5%) applicable to all products.
    * Used in price or tax calculations.
    */
   static taxRate = 0.05;
+  sku: string; // Product stock keeping unit
   name: string;
   price: number;
-  sku: string;
 
   constructor(sku: string, name: string, price: number) {
+    this.sku = sku;
     this.name = name;
     this.price = price;
-    this.sku = sku;
   }
   // Method that calculates the final price of the product with tax
   getPriceWithTax(): number {
@@ -21,6 +21,6 @@ class Product {
   }
   // method that returns a formatted string with the product’s details
   displayDetails(): string {
-    return `${this.name} (SKU: ${this.sku}) costs $${this.price}.`;
+    return `sku: ${this.sku} name: ${this.name}  costs: $${this.price}.`;
   }
 }
